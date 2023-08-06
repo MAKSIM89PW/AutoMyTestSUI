8a0247bdac7c3136841029c3d06f78f65777600d
MAKSIM89PW <123>
2023-08-06 22:28:24 +0300

    Created a post view

--- AutoMyTestSUI/AutoMyTestSUI/PostCard.swift
+++ AutoMyTestSUI/AutoMyTestSUI/PostCard.swift
@@ -15,6 +15,7 @@ struct PostCard: View {
 
 struct PostCard_Previews: PreviewProvider {
     static var previews: some View {
-        PostCard()
+        PostCard(image: "http://am111.05.testing.place/uploads/user/32/auto/32/post/45/be3705db6c580f82430279b6f75b4b93.jpg",
+                 date: "2018-12-21", postText: "Добрый день, дорогие друзья! Как-то дождь пна улице, тоска зеленая. Решил пофантазировать…))))\r\nНе не, извращенцы, я не об этом)))))\r\nЯ о том, как бы мог выглядеть Лансер. И вот я нафантазировал…\r\nЖду комментариев!\r\nСпасибо, что заходите!", likesCount: 8, commentsCount: 1)
     }
 }

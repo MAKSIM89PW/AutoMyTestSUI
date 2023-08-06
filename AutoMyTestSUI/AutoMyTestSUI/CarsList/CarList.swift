c32669f13eab2ee2df9b00c082be08ec9ab5ffcb
MAKSIM89PW <123>
2023-08-06 19:16:35 +0300

    Added infinite scroll and changed the entities that affect it

---
+++ AutoMyTestSUI/AutoMyTestSUI/CarsList/CarList.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  AutoMyTestSUI
+//
+//  Created by Максим Нурутдинов on 06.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var carsListViewModel = CarsListViewModel()
+    @StateObject var carViewModel = CarViewModel()
+    @StateObject var postViewModel = PostCarViewModel()
+    
+    @State private var selectedCarId: Int?
+    
+    var body: some View {
+        NavigationStack {
+            Text("Автомобилка")
+                .font(.title)
+                .fontWeight(.bold)
+            
+            ScrollView(.vertical) {
+                LazyVStack {
+                    ForEach(carsListViewModel.carsList, id: \.id) { car in
+                        NavigationLink(
+                            destination: CarScreen(carViewModel: carViewModel, postsViewModel: postViewModel)
+                                .onAppear {
+                                    carViewModel.fetchCarInformation(car: car.id)
+                                    postViewModel.fetchCarPostsInformation(car: car.id)
+                                },
+                            tag: car.id,
+                            selection: $selectedCarId
+                        ) {
+                            CarsListCard(image: car.images,
+                                         brand: car.brandName,
+                                         model: car.modelName,
+                                         year: car.year,
+                                         transmission: car.transmissionName)
+                                .padding(.vertical, 5)
+                        }
+                        .onAppear {
+                            if car.id == carsListViewModel.carsList.last?.id {
+                                carsListViewModel.fetchMoreCarsInList()
+                            }
+                        }
+                    }
+                }
+                
+                if carsListViewModel.isLoading {
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                        .padding(.all)
+                }
+            }
+        }
+        .onAppear {
+            carsListViewModel.fetchCarsList()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- AutoMyTestSUI/AutoMyTestSUI/Network/NetworkManager.swift
+++ AutoMyTestSUI/AutoMyTestSUI/Network/NetworkManager.swift
@@ -9,22 +9,51 @@ import Foundation
 import Combine
 
 private enum Urls {
-    static let carsList = "http://am111.05.testing.place/api/v1/cars/list"
+    static let carsList = "http://am111.05.testing.place/api/v1/cars/list?page="
+    static let car = "http://am111.05.testing.place/api/v1/car/"
 }
 
 class NetworkManager: ObservableObject {
     static let shared = NetworkManager()
-
-    func getCarsList() -> AnyPublisher<[CarsListItem], Error> {
-        guard let url = URL(string: Urls.carsList) else {
+    
+    func getCarsList(for page: Int) -> AnyPublisher<[CarsListItem], Error> {
+        let buildedUrl = Urls.carsList + String(page)
+        guard let url = URL(string: buildedUrl) else {
             return Fail(error: NSError(domain: "Error", code: -1)).eraseToAnyPublisher()
         }
-
         return URLSession.shared.dataTaskPublisher(for: url)
             .map {
                 $0.data
             }
             .decode(type: [CarsListItem].self, decoder: JSONDecoder())
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+    
+    func getCarInfo(for id: Int) -> AnyPublisher<Car, Error> {
+        let buildedUrl = Urls.car + String(id)
+        guard let url = URL(string: buildedUrl) else {
+            return Fail(error: NSError(domain: "Error", code: -1)).eraseToAnyPublisher()
+        }
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map {
+                $0.data
+            }
+            .decode(type: Car.self, decoder: JSONDecoder())
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+    
+    func getCarPosts(for id: Int,_ page: Int) -> AnyPublisher<PostsForCar, Error> {
+        let buildedUrl = Urls.car + String(id) + String("/posts?page=") + String(page)
+        guard let url = URL(string: buildedUrl) else {
+            return Fail(error: NSError(domain: "Error", code: -1)).eraseToAnyPublisher()
+        }
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map {
+                $0.data
+            }
+            .decode(type: PostsForCar.self, decoder: JSONDecoder())
             .receive(on: RunLoop.main)
             .eraseToAnyPublisher()
     }

5f7885eb4b26628d56503c4a20a5a16a79b952ae
MAKSIM89PW <123>
2023-08-06 21:37:05 +0300

    Created a page to display profile and posts

---
+++ AutoMyTestSUI/AutoMyTestSUI/PostCard.swift
@@ -0,0 +1,20 @@
+//
+//  PostCard.swift
+//  AutoMyTestSUI
+//
+//  Created by Максим Нурутдинов on 06.08.2023.
+//
+
+import SwiftUI
+
+struct PostCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostCard_Previews: PreviewProvider {
+    static var previews: some View {
+        PostCard()
+    }
+}

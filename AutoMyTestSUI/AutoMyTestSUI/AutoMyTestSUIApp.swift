eecb976f9befdab6d84f065f50e3e03dd555992b
MAKSIM89PW <123>
2023-08-06 17:24:09 +0300

    Initiating commit

---
+++ AutoMyTestSUI/AutoMyTestSUI/AutoMyTestSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoMyTestSUIApp.swift
+//  AutoMyTestSUI
+//
+//  Created by Максим Нурутдинов on 06.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AutoMyTestSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
